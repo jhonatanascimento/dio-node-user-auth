678aacb1c8d1a271ce90cdee17d0c7b8ee1c94c6
jhonata <jhonata_ti@outlook.com>
2022-02-02 19:33:35 -0300

    adicionado todas as rotas de CRUD em usuários

--- live01/src/index.ts
+++ live01/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Rodei");
+import express, { Request, Response, NextFunction, urlencoded } from 'express';
+import statusRoutes from './routes/status.route';
+import usersRoute from './routes/users.route';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(usersRoute);
+app.use(statusRoutes);
+
+app.listen(3000, () => {
+    console.log('Aplicação rodando na porta 3000')
+})
+
+
+
